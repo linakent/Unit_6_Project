24d53082df08c904fb62ee7beabf08a7701fd930
BT_2S14_12 <linak9@nycstudents.net>
2025-01-28 10:01:39 -0500

    Made GroupHand class

---
+++ src/GroupHand.java
@@ -0,0 +1,12 @@
+public class GroupHand {
+
+    public String card1;
+    public String card2;
+    public String card3;
+    public String card4;
+    public String card5;
+
+    public GroupHand (String playerHand){
+
+    }
+}
