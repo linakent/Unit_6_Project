8db2845bc146aab836a1a456f20e2ab106e55163
BT_2S14_11 <tyhdjn@tredh.edu>
2025-01-31 10:01:39 -0500

    restore version

--- src/PartOne.java
+++ src/PartOne.java
@@ -19,37 +19,28 @@ public class PartOne {
     }
 
     public void getPartOneAnswer(String[] inputArray) {
-        PartTwo partTwo = new PartTwo();
         for (int x = 0; x < inputArray.length; x ++) {
             GroupHand hand = new GroupHand(inputArray[x]);
             String handType = hand.assignHand();
-            System.out.println("hand " + x + " = " + handType);
             if (handType.equals("five")) {
-                partTwo.fiveOfAKindArray.add(partTwo.in2DArray(hand.cards, partTwo.getBet(inputArray[x])));
                 fiveOfAKind ++;
             }
             else if (handType.equals("four")) {
-                partTwo.fourOfAKindArray.add(partTwo.in2DArray(hand.cards, partTwo.getBet(inputArray[x])));
                 fourOfAKind ++;
             }
             else if (handType.equals("full")) {
-                partTwo.fullHouseArray.add(partTwo.in2DArray(hand.cards, partTwo.getBet(inputArray[x])));
                 fullHouse ++;
             }
             else if (handType.equals("three")) {
-                partTwo.threeOfAKindArray.add(partTwo.in2DArray(hand.cards, partTwo.getBet(inputArray[x])));
                 threeOfAKind ++;
             }
             else if (handType.equals("two")) {
-                partTwo.twoPairArray.add(partTwo.in2DArray(hand.cards, partTwo.getBet(inputArray[x])));
                 twoPair++;
             }
             else if (handType.equals("one")) {
-                partTwo.onePairArray.add(partTwo.in2DArray(hand.cards, partTwo.getBet(inputArray[x])));
                 onePair ++;
             }
             else if (handType.equals("high")) {
-                partTwo.highCardArray.add(partTwo.in2DArray(hand.cards, partTwo.getBet(inputArray[x])));
                 highCard ++;
             }
         }
